ff130d7dc70ebb211a7d0277122ad475b0fb9d13
parcocheung <parcocheung01@gmail.com>
2023-11-09 23:01:24 +0000

    lab7

---
+++ credentials.php
@@ -0,0 +1,6 @@
+<?php
+$servername = '192.168.110.132';     //if apache on diff server from db or using Docker, set IP address
+$dbname = 'employees';              //which db you're going to use
+$username = 'phpuser';
+$password = 'dbuser123';
+?>
